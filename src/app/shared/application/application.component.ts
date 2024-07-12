fb07a62a4d64c9cb4bab4bd46cfcb2695d24e15d
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-07-12 17:33:27 -0500

    Added dropdown menu for application options

--- src/app/shared/application/application.component.ts
+++ src/app/shared/application/application.component.ts
@@ -3,11 +3,13 @@ import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { SharedModule } from '../shared.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-application',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, SharedModule],
+  imports: [MatButtonModule, MatCardModule, SharedModule, MatIconModule, MatMenuModule],
   templateUrl: './application.component.html',
   styleUrl: './application.component.css'
 })
