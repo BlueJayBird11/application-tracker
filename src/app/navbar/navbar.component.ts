4e20cc84c39077cef98cafaee25f0bbff85eea21
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-08-01 13:48:32 -0500

    Getting closer to displaying correct errors

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormGroupDirective, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, FormsModule, NgForm, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -12,7 +12,7 @@ import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input'
 import { MatSelectModule } from '@angular/material/select';
 import { ApplicationsService } from '../shared/application/applications.service';
-import { passwordValidator, urlValidator } from '../shared/validators/validators';
+import { confirmPasswordValidator, passwordValidator, urlValidator } from '../shared/validators/validators';
 import { UserService } from '../shared/user.service';
 
 @Component({
@@ -55,8 +55,9 @@ export class CreateAccountDialog {
   enteredPassword1: string = '';
   enteredPassword2: string = '';
 
-  emailFormControl = new FormControl('', [Validators.required, Validators.email, Validators.maxLength(64)]);
-  password2FormControl = new FormControl('', [Validators.required, passwordValidator(this.enteredPassword1)]);
+  // emailFormControl = new FormControl('', [Validators.required, Validators.email, Validators.maxLength(64)]);
+  // passwordFormControl = new FormControl('', [Validators.required, Validators.maxLength(64)]);
+  // password2FormControl = new FormControl('', [Validators.required, passwordValidator(this.enteredPassword1), Validators.maxLength(64)]);
 
   hide = signal(true);
   hide2 = signal(true);
@@ -77,6 +78,12 @@ export class CreateAccountDialog {
 
 
 
+  // form: FormGroup = new FormGroup({
+  //   email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(64)]),
+  //   password1: new FormControl<string>('', [Validators.required]),
+  //   password2: new FormControl<string>('', [Validators.required]),
+  // });
+
   form: FormGroup;
 
   passwordsAreEqual: boolean = true;
@@ -86,22 +93,79 @@ export class CreateAccountDialog {
     private applicationsService: ApplicationsService,
     private fb: FormBuilder
   ) {
-    this.form = this.fb.group({
-      email: ['', Validators.required, Validators.email, Validators.maxLength(64)],
-      password1: ['', Validators.required, Validators.maxLength(64)],
-      password2: ['', Validators.required, Validators.maxLength(64)],
+    this.form = new FormGroup(
+      {
+        email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(64)]),
+        password1: new FormControl<string>('', [Validators.required, Validators.minLength(5)]),
+        password2: new FormControl<string>('', [Validators.required]),
+      },
+      { validators: confirmPasswordValidator }
+    );
+  }
+
+  checkFormErrors(formGroup: FormGroup): void {
+    Object.keys(formGroup.controls).forEach(key => {
+      const controlErrors: ValidationErrors | null | undefined = formGroup.get(key)?.errors;
+      if (controlErrors != null && controlErrors != undefined) {
+        Object.keys(controlErrors).forEach(keyError => {
+          console.log('Key control: ' + key + ', keyError: ' + keyError + ', error value: ', controlErrors[keyError]);
+        });
+      }
+    });
+
+    const formGroupErrors: ValidationErrors | null = formGroup.errors;
+    if (formGroupErrors) {
+      Object.keys(formGroupErrors).forEach(keyError => {
+        console.log('FormGroup error: ' + keyError + ', error value: ', formGroupErrors[keyError]);
+      });
+    }
+  }
+
+  get emailFormControl() {
+    return this.form.get('email');
+  }
+
+  get password1FormControl() {
+    return this.form.get('password1');
+  }
+
+  get password2FormControl() {
+    return this.form.get('password2');
+  }
+
+  get formGroupErrors() {
+    return this.form.errors;
+  }
+
+  markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach(control => {
+      if (control instanceof FormControl) {
+        control.markAsTouched();
+      } else if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      }
     });
   }
 
-  onSignUpButtonClick(form: NgForm) {
-    if (form.invalid) {
-      form.control.markAllAsTouched(); // Mark all fields as touched to trigger validation
-      return;
-    }
-    if (this.enteredPassword1 !== this.enteredPassword2)
+  onSignUpButtonClick() {
+    // console.log(this.password2FormControl.errors);
+    console.log('Errors: ' + this.form.errors);
+    console.log('Invalid: ' + this.form.invalid);
+    this.checkFormErrors(this.form);
+    // if (this.form.invalid) {
+    //   form.control.markAllAsTouched(); // Mark all fields as touched to trigger validation
+    //   return;
+    // }
+    // if (this.enteredPassword1 !== this.enteredPassword2)
+    // {
+    //   this.passwordsAreEqual = false;
+    //   form.control.markAllAsTouched();
+    //   return;
+    // }
+    console.log("NoMatch: " + this.form.hasError('PasswordNoMatch'));
+    if (this.form.invalid)
     {
-      this.passwordsAreEqual = false;
-      form.control.markAllAsTouched();
+      this.markFormGroupTouched(this.form);
       return;
     }
 
--- src/app/shared/validators/validators.ts
+++ src/app/shared/validators/validators.ts
@@ -19,3 +19,12 @@ export function passwordValidator(password1: string): ValidatorFn {
     return null;
   };
 }
+
+// Source: https://blog.bitsrc.io/implementing-confirm-password-validation-in-angular-with-custom-validators-6acd01cb0288
+export const confirmPasswordValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  return control.value.password1 === control.value.password2
+    ? null
+    : { PasswordNoMatch: true };
+};
